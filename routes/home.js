68387a5c1245df27b082635d32c6cc40fd44445c
buiminhhoat <official.buiminhhoat@gmail.com>
2023-04-29 22:03:35 +0700

    Add Require Login

--- routes/home.js
+++ routes/home.js
@@ -3,14 +3,31 @@ const router = express.Router();
 module.exports = router;
 
 const multer = require('multer');
+const {verify} = require("jsonwebtoken");
 const upload = multer({ dest: 'uploads/' });
+
+function requireLogin(req, res, next) {
+    const token = req.cookies['token'];
+    console.log(token);
+    if (token === undefined) {
+        res.redirect('/login');
+        return;
+    }
+    const email = verify(token, process.env.TOKEN_SECRET).email;
+    if (email === "") {
+        res.redirect('/login');
+    } else {
+        next();
+    }
+}
+
 
 router.get('/', async (req, res) => {
     res.render('./ejs/home.ejs');
 })
 
 router.post('/api/', require('./view').view);
-router.get('/create', (req, res) => {
+router.get('/create', requireLogin, (req, res) => {
     let content= "";
     let post_id= "";
     let title = "";
@@ -22,17 +39,17 @@ router.get('/create', (req, res) => {
     res.render("./ejs/index.ejs", { content, post_id, title });
 })
 
-router.post('/api/create', require('./view').create);
+router.post('/api/create', requireLogin, require('./view').create);
 
-router.post('/api/download', require('./download.js'));
+router.post('/api/download', requireLogin, require('./download.js'));
 
-router.post('/upload', upload.single('htmlfile'), require('./upload.js'));
+router.post('/upload', requireLogin, upload.single('htmlfile'), require('./upload.js'));
 
-router.post('/api/downloadFileWord', require('./downloadFileWord.js'));
+router.post('/api/downloadFileWord', requireLogin, require('./downloadFileWord.js'));
 
-router.post('/api/refresh_token', require('./refresh_token').refresh_token);
+router.post('/api/refresh_token', requireLogin, require('./refresh_token').refresh_token);
 
-router.post('/savesql', require('./savesql.js'));
+router.post('/savesql', requireLogin, require('./savesql.js'));
 
 router.get('/login', (req, res) => {
     res.render("./hbs/login.hbs");
@@ -40,7 +57,7 @@ router.get('/login', (req, res) => {
 
 router.post('/api/login', require('./authentication').login);
 
-router.get('/api/logout',  require('./authentication').logout);
+router.get('/api/logout', require('./authentication').logout);
 
 router.get('/register', (req, res) => {
     res.render("./hbs/register.hbs");
